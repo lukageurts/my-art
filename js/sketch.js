dd83a9449bdd0d0654de8e187d36264d63b1adf0
lukageurts <71170846+lukageurts@users.noreply.github.com>
2020-09-15 15:07:41 +0200

    Added Index.html and js files

---
+++ js/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  // put setup code here
+  
+  createCanvas(200,200);
+  background(100);
+  print("dit is een set-up");
+  rectMode(CENTER); 
+  ellipse(100,100,200,200);
+  fill(255,0,0);
+  rect(100,100,20,20);
+
+  fill(0);
+  textAlign(CENTER, CENTER);
+  text("dit is het midden",100,100);
+
+}
+
+function draw() {
+  // put drawing code here
+}
